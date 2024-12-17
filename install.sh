3726b4f7561b49dd3f22d915beac3ed847b8273e
Rozhbas Leader Blade Never Die <103634965+RozhBasXYZ@users.noreply.github.com>
2024-12-17 14:10:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ pkg update -y && pkg upgrade -y
 echo -e "\n install new repository: tur-repo ...\n"
 pkg install tur-repo -y && pkg update -y
 echo -e "\n install package and dependencies ...\n"
-pkg install git nano openssl play-audio binutils build-essential python -y
-echo 'alias python3="python"' >> $PREFIX/etc/bash.bashrc
-echo 'alias pip="python -m pip"' >> $PREFIX/etc/bash.bashrc
+pkg install git nano openssl binutils build-essential python3.10 -y
+echo 'alias python3="python3.10"' >> $PREFIX/etc/bash.bashrc
+echo 'alias pip="python3.10 -m pip"' >> $PREFIX/etc/bash.bashrc
 source $PREFIX/etc/bash.bashrc
 echo -e "\n cloning github repository -> 'instaroz' ...\n"
 git clone https://github.com/RozhBasXYZ/NEW-INSTAROZ
 echo -e "\n install module and requirements ...\n"
-cd INSTAROZ && python -m pip install --upgrade pip && python -m pip install wheel && python -m pip install -r requirements.txt
+cd NEW-INSTAROZ && python3.10 -m pip install --upgrade pip && python3.10 -m pip install wheel && python3.10 -m pip install -r requirements.txt
 find data/ -name '*.md' -delete
 echo -e "\n sedang melakukan build.py"
-python build.py
+python3.10 build.py
 echo -e "\n build sukses, berikan izin storage ( y / izinkan )"
 termux-setup-storage
 echo -e "\n sukses memberikan izin, sc segera siap..."
 cd /sdcard && mkdir STAROZ 
-python run.py
+python3.10 run.py
