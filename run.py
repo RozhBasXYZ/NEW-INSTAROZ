ce160c876719c0833d1a3e1a7c6068477b92d262
Rozhbas Leader Blade Never Die <103634965+RozhBasXYZ@users.noreply.github.com>
2025-10-22 21:16:00 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os, instaroz as rozh
+import os, projekig as rozh
 try: os.system("git pull"); os.system("python3.10 build.py")
 except: print("[!] anda memakai versi terbaru")
 rozh.main_line()
